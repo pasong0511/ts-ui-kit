833abd1bad184ee0b1821a8b3f8b01749c60c171
SONGHEE <pasong0511@gmail.com>
2024-01-18 00:01:16 +0900

    키보드 이벤트 이넘 파일 추가

---
+++ src/enums/event.ts
@@ -0,0 +1,15 @@
+export enum EVENT_KEY {
+    Tab = "Tab",
+    Enter = "Enter",
+    Spacebar = " ",
+    Esc = "Escape",
+    ArrowDown = "ArrowDown",
+    ArrowLeft = "ArrowLeft",
+    ArrowRight = "ArrowRight",
+    ArrowUp = "ArrowUp",
+    Backspace = "Backspace",
+    Down = "Down",
+    Left = "Left",
+    Right = "Right",
+    Up = "Up",
+}
